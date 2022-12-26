2d306351e9321496e5a65ba83f4aba0c9f2678f3
extroblade <mr.ovs0ford@gmail.com>
2022-12-26 14:50:11 +0400

    simple currencies converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import Block from "./Block";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [fromCur, setFromCur] = useState('RUB');
+    const [toCur, setToCur] = useState('USD');
+    const [fromPrice, setFromPrice] = useState(0);
+    const [toPrice, setToPrice] = useState(1);
+
+    const ratesRef = useRef({})
+
+    useEffect(() => {
+        fetch('https://www.cbr-xml-daily.ru/latest.js')
+            .then(res => res.json())
+            .then(json =>  {
+                ratesRef.current = json.rates;
+                onChangeToPrice(1);
+            })
+            .catch(e =>  console.log('Error: '+ e))
+    }, [])
+
+    const onChangeFromPrice = (value) => {
+        let price
+
+        if(fromCur==='RUB'){
+            price = value*ratesRef.current[toCur];
+        } else if (toCur==='RUB'){
+            price = value/ratesRef.current[fromCur]
+        } else {
+            price = value*ratesRef.current[toCur]/ratesRef.current[fromCur]
+        }
+
+        setFromPrice(value);
+        setToPrice(price);
+    }
+
+    const onChangeToPrice = (value) => {
+        let price
+
+        if(toCur==='RUB'){
+            price = value*ratesRef.current[fromCur];
+        } else if (fromCur==='RUB'){
+            price = value/ratesRef.current[toCur]
+        } else {
+            price = value*ratesRef.current[fromCur]/ratesRef.current[toCur]
+        }
+        setFromPrice(price);
+        setToPrice(value);
+    }
+
+    useEffect(() => {
+        onChangeFromPrice(fromPrice)
+        onChangeToPrice(toPrice)
+    }, [fromCur, toCur])
+
+
+    return (
+        <div className="App">
+            <Block
+                value={fromPrice}
+                currency={fromCur}
+                onChangeCurrency={setFromCur}
+                onChangeValue={onChangeFromPrice}
+            />
+            <Block
+                value={toPrice}
+                currency={toCur}
+                onChangeCurrency={setToCur}
+                onChangeValue={onChangeToPrice}
+            />
+        </div>
+    );
+};
 
 export default App;
